33f1f4c5413d5876909fe18eb9d2d60b48f31224
aaraomelo <aarao.melo.lopes@gmail.com.br>
2026-04-30 10:49:58 -0400

    feat(arqueiro): export startArqueiro headless (v0.2.1)

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import './styles.css'
 
 export { ChatWindow } from './chat-core/components/ChatWindow'
 export type { ChatWindowProps, ArqueiroProps } from './chat-core/components/ChatWindow'
+
+// Headless: spawn arqueiro sem renderizar ChatWindow (para uso global invisível)
+export { startArqueiro } from './arqueiro'
+export type { ArqueiroConfig, ArqueiroHandle } from './arqueiro'
